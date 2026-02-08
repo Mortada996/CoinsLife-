25de3991f7536224734149ae338f058a5183cf47
Mortada996 <abderrahmankhattabi83@gmail.com>
2026-02-08 11:52:35 +0100

    Update CoinsLifeClient.kt

--- src/main/kotlin/com/coinslife/CoinsLifeClient.kt
+++ src/main/kotlin/com/coinslife/CoinsLifeClient.kt
@@ -3,14 +3,16 @@ package com.coinslife
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry
 import net.minecraft.client.MinecraftClient
-import net.minecraft.client.util.math.MatrixStack
-import net.minecraft.item.ItemStack
-import net.minecraft.client.render.VertexConsumerProvider
-import net.minecraft.client.render.model.json.ModelTransformationMode
+import net.minecraft.util.Identifier
 
 object CoinsLifeClient : ClientModInitializer {
     override fun onInitializeClient() {
-        // This is where the magic happens to render the config image on the coin
-        // We will add the dynamic rendering logic here in the next step
+        // This registers the client-side logic to handle custom item textures
+        // In Fabric, dynamic item textures from external files (config) 
+        // usually require a Dynamic Texture Registration.
+        
+        LOGGER_CLIENT.info("CoinsLife Client-Side initialized.")
     }
+
+    private val LOGGER_CLIENT = org.slf4j.LoggerFactory.getLogger("coinslife-client")
 }
