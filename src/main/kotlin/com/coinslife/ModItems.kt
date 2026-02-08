f9cc7532d9339623802afe5549b1a31008648b64
Mortada996 <abderrahmankhattabi83@gmail.com>
2026-02-08 11:30:56 +0100

    Create ModItems.kt

---
+++ src/main/kotlin/com/coinslife/ModItems.kt
@@ -0,0 +1,14 @@
+package com.coinslife
+
+import net.minecraft.item.Item
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.util.Identifier
+
+object ModItems {
+    val PLAYER_COIN = Item(Item.Settings().maxCount(64))
+
+    fun register() {
+        Registry.register(Registries.ITEM, Identifier.of("coinslife", "player_coin"), PLAYER_COIN)
+    }
+}
